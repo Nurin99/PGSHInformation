c8137ef503f046ba79578af4ac2eabbd4df6318c
Nurin99 <101875874+Nurin99@users.noreply.github.com>
2023-09-26 12:31:47 +0800

    Create level8.php

---
+++ level8.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+/* Styling for the appointment table */
+.appointment-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+/* Center the vertical lines for all table cells */
+.appointment-table th,
+.appointment-table td {
+  border: 1px solid #ccc;
+  padding: 8px;
+  text-align: center;
+  vertical-align: middle; /* Center-align the text vertically */
+}
+
+.appointment-table th {
+  background-color: #f0f0f0; 
+}
+/* Styles for the container div */
+.container {
+      text-align: center; /* Center-align the content inside the container */
+    }
+/* Styles for the print button */
+#printButton {
+      background-color: #007bff;
+      color: #fff;
+      border: none;
+      padding: 10px 15px;
+      font-size: 16px;
+      cursor: pointer;
+      margin-right: 10px;
+      float: right; /* Float the button to the left */
+    }
+    @media print {
+  #printButton {
+    display: none;
+  }
+  .red-box {
+    background-color: red;
+    color: white;
+  }
+  #searchInput {
+    display: none;
+  }
+}
+
+    .appointment-table td:first-child {
+    text-transform: uppercase;
+  }
+  
+.red-box {
+  background-color: red; /* Set background color to red */
+  color: white; /* Set text color to white for better visibility */
+}
+
+.appointment-table td:nth-child(odd),
+.appointment-table th:nth-child(odd) {
+  width: 50%; /* Set a fixed width of 50% for odd-numbered columns (left column) */
+}
+
+.appointment-table td:nth-child(even),
+.appointment-table th:nth-child(even) {
+  width: 50%; /* Set a fixed width of 50% for even-numbered columns (right column) */
+}
+
+</style>
+</head>
+<body>
+<!-- Main content -->
+<div class="main">
+     <!-- Print button -->
+     <button  id="printButton" onclick="printPage()">🖨️ Print</button>
+
+     <h2 style="text-align: center;">KPJ PASIR GUDANG SPECIALIST HOSPITAL DIRECTORY</h2>
+  <p style="text-align: center;">UPDATED AS AT: 03/06/23</p>
+  <p style="text-align: center;">LEVEL 8</p>
+  <input type="text" id="searchInput" placeholder="Search...">
+  <table class="appointment-table">
+
+ <!-- Include LEVEL 8 content here -->
+<!-- DIAMOND WARD -->
+<tr>
+  <th colspan="3">DIAMOND WARD</th>
+  
+</tr>
+<tr>
+  <td>Puan Suzana Ismail (UM)</td>
+  <td>5838</td>
+</tr>
+<tr>
+  <td>Counter 1</td>
+  <td>5842</td>
+</tr>
+<tr>
+  <td>Counter 2</td>
+  <td>5844</td>
+</tr>
+</table>
+</div>
+  <!-- JavaScript for printing -->
+  <script>
+    // Function to trigger the print dialog
+    function printPage() {
+      window.print();
+    }
+  // Function to filter the table rows based on the search input
+  function filterTable() {
+    const input = document.getElementById('searchInput');
+    const filter = input.value.toLowerCase();
+    const table = document.querySelector('.appointment-table');
+    const rows = table.querySelectorAll('tr');
+
+    rows.forEach(row => {
+      const cells = row.querySelectorAll('td, th');
+      let found = false;
+
+      cells.forEach(cell => {
+        if (cell.textContent.toLowerCase().includes(filter)) {
+          found = true;
+        }
+      });
+    if (row.querySelector('th')) {
+      // This row is a section title
+      if (found) {
+        // If the section title matches the search, show the entire section
+        currentSection = row;
+        row.style.display = '';
+      } else {
+        // Hide the section title if it doesn't match the search
+        row.style.display = 'none';
+        currentSection = null;
+      }
+    } else if (found || (currentSection && currentSection.style.display !== 'none')) {
+      // Show the row if it matches the search or if it's part of the current visible section
+      row.style.display = '';
+    } else {
+      // Hide the row if it doesn't match the search and is not part of the current visible section
+      row.style.display = 'none';
+    }
+  });
+}
+
+  // Attach an event listener to the input field
+  document.getElementById('searchInput').addEventListener('input', filterTable)
+
+  </script>
+</body>
+</html>
